1de017cfa28fd8e7121484907fd6f2a01dbd9f55
Artyom Illiy <artemilliy@gmail.com>
2025-10-04 17:02:32 +0300

    feat(vite.config & vercel): настроить алиасы проекта, сборщик и конфиг для деплоя на vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,58 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/",
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@api": "/src/api",
+      "@assets": "/src/assets",
+      "@components": "/src/components",
+      "@config": "/src/config",
+      "@hooks": "/src/hooks",
+      "@pages": "/src/pages",
+      "@schemas": "/src/schemas",
+      "@store": "/src/store",
+      "@styles": "/src/styles",
+      "@types": "/src/types",
+      "@utils": "/src/utils",
+    },
+  },
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            const chunks = {
+              vendor: ["react", "react-dom"],
+              router: ["react-router-dom"],
+              redux: ["@reduxjs/toolkit", "react-redux"],
+            };
+
+            for (const [chunkName, modules] of Object.entries(chunks)) {
+              if (
+                modules.some((module) => id.includes(`node_modules/${module}`))
+              ) {
+                return chunkName;
+              }
+            }
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    host: true,
+  },
+
+  preview: {
+    port: 4173,
+    host: true,
+  },
+});
