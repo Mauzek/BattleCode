cb0fcc0c3ad4fcb90fb373c7e3ed112678459839
Artyom Illiy <artemilliy@gmail.com>
2025-10-04 18:18:06 +0300

    feat(app): обернуть навигацию приложения в layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
+import Layout from "./components/layout/layout";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<div>
-        <h1>Hello, BattelCode!</h1>
-        <p>Welcome to your React + TypeScript + Vite project.</p>
-      </div>} />
-    </Routes>
-  )
+    <Layout>
+      <Routes>
+        <Route path="/" element={<div>Home Page</div>}/>
+      </Routes>
+    </Layout>
+  );
 }
 
-export default App
+export default App;
