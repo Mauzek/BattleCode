79a71b966d4780d59af1fc6140df3989b51222c3
Artyom Illiy <artemilliy@gmail.com>
2025-10-04 18:16:54 +0300

    feat(mobileNavBar): добавить заглушку mobileNavBar

---
+++ src/components/layout/mobileNavBar/mobileNavBar.tsx
@@ -0,0 +1,11 @@
+import styles from './mobileNavBar.module.scss';
+
+export const MobileNavBar = () => {
+  return (
+    <div className={styles.navBar}>
+        <nav className={styles.navBar__nav}>
+            MobileNavBar
+        </nav>
+    </div>
+  )
+}
