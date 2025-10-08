863b3cf08f72b11d0b4f6712b7d9f33cbec29d18
Artyom Illiy <artemilliy@gmail.com>
2025-10-08 18:30:28 +0300

    feat(mobileNavBar): сделать первую версию mobileNavBar

--- src/components/layout/mobileNavBar/mobileNavBar.tsx
+++ src/components/layout/mobileNavBar/mobileNavBar.tsx
@@ -1,11 +1,41 @@
-import styles from './mobileNavBar.module.scss';
+import { NavLink } from "react-router-dom";
+import styles from "./mobileNavBar.module.scss";
+import { LuHouse, LuBook, LuUser, LuCalendar, LuLayers } from "react-icons/lu";
 
-export const MobileNavBar = () => {
+interface Tab {
+  icon: React.ReactNode;
+  path: string;
+  end?: boolean;
+}
+
+export const MobileNavBar: React.FC = () => {
+  const tabs: Tab[] = [
+    { icon: <LuHouse />, path: "/", end: true },
+    { icon: <LuBook />, path: "/courses", end: true },
+    { icon: <LuUser />, path: "/user/boby", end: true },
+    { icon: <LuCalendar />, path: "/calendar", end: true },
+    { icon: <LuLayers />, path: "/courses/my" },
+  ];
+
   return (
     <div className={styles.navBar}>
-        <nav className={styles.navBar__nav}>
-            MobileNavBar
-        </nav>
+      <nav className={styles.navBar__nav}>
+        {tabs.map((tab, index) => (
+          <NavLink
+            key={index}
+            to={tab.path}
+            end={tab.end}
+            onClick={() => window.scrollTo(0,0)}
+            className={({ isActive }) =>
+              `${styles.navBar__tab} ${
+                isActive ? styles["navBar__tab--active"] : ""
+              }`
+            }
+          >
+            {tab.icon}
+          </NavLink>
+        ))}
+      </nav>
     </div>
-  )
-}
+  );
+};
--- src/pages/profilePage/index.tsx
+++ src/pages/profilePage/index.tsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 export const ProfilePage = () => {
   const { username } = useParams();
   return (
-    <main>
+    <main style={{ height: "1000px" }}>
       <h1>Профиль {username}</h1>
     </main>
   );
