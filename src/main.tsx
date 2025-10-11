cba1f033fddf3a7843c44489122e60a3680eba30
IcQxp <batla3080@gmail.com>
2025-10-11 23:45:42 +0300

    added simple state for auth and profile

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import "./index.scss";
 import "@/styles/fonts.scss";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/index.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+    <Provider store={store}>
       <App />
+    </Provider>
     </BrowserRouter>
   </StrictMode>
 );
